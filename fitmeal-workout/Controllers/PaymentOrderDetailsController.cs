cc555017fde871cddba4b1ab67c5ee49b6265c01
anoopec110 <52349515+anoopec110@users.noreply.github.com>
2026-01-15 15:03:34 +0530

    adding admin page api calls

---
+++ fitmeal-workout/Controllers/PaymentOrderDetailsController.cs
@@ -0,0 +1,46 @@
+﻿using fitmeal_workout.DatabaseContext;
+using fitmeal_workout.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace fitmeal_workout.Controllers
+{
+    [ApiController]
+    [Route("/api/orderDetails")]
+    public class PaymentOrderDetailsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentOrderDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("FetchPaymentDetails")]
+        public async Task<IActionResult> FetchPaymentDetails()
+        {
+            try
+            {
+                var paymentDetails = await _context.ordersDetails
+                    .AsNoTracking()
+                    .OrderByDescending(x => x.CreatedAt)
+                    .Select(payment => new PaymentDetails()
+                    {
+                        RazorpayOrderId = payment.RazorpayOrderId,
+                        Amount = payment.Amount,
+                        paymentStatus = payment.paymentStatus,
+                        Phone = payment.Phone,
+                        Name = payment.Name,
+                        Email = payment.Email,
+                        PlanCode = payment.PlanCode,
+                        CreatedAt = payment.CreatedAt
+
+                    }).ToListAsync();
+                return Ok(paymentDetails);
+            }
+            catch (Exception ex) {
+                return StatusCode(500,ex.Message);
+            }
+        }
+    }
+}
---
+++ fitmeal-workout/Models/PaymentDetails.cs
@@ -0,0 +1,19 @@
+﻿namespace fitmeal_workout.Models
+{
+    public class PaymentDetails
+    {
+        public string RazorpayOrderId { get; set; }
+
+        public string PlanCode { get; set; }
+        public decimal Amount { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string paymentStatus { get; set; }
+
+
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
